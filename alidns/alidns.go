f806361c15c76b68f045e06b9e5ef385b80671f4
shynome <shynome@gmail.com>
2019-02-14 21:58:05 +0800

    vvvv

--- alidns/alidns.go
+++ alidns/alidns.go
@@ -2,19 +2,13 @@ package alidns
 
 import (
 	"errors"
-	"os"
-	"strings"
 
 	"github.com/mholt/caddy/caddytls"
-	"github.com/xenolf/lego/challenge/dns01"
 	"github.com/xenolf/lego/providers/dns/alidns"
 )
 
 func init() {
 	caddytls.RegisterDNSProvider("alidns", NewDNSProvider)
-	if nameservers := os.Getenv("LEGO_NAMESERVERS"); nameservers != "" {
-		dns01.AddRecursiveNameservers(dns01.ParseNameservers(strings.Split(nameservers, ",")))
-	}
 }
 
 // NewDNSProvider returns a new Aliyun DNS challenge provider.
@@ -26,7 +20,9 @@ func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
 		config := alidns.NewDefaultConfig()
 		config.APIKey = credentials[0]
 		config.SecretKey = credentials[1]
-		return alidns.NewDNSProviderConfig(config)
+		config.Nameservers = "dns21.hichina.com,dns22.hichina.com"
+		dnsProvider, err := alidns.NewDNSProviderConfig(config)
+		return dnsProvider, err
 	default:
 		return nil, errors.New("invalid credentials length")
 	}
